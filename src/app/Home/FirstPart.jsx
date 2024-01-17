055fcc4c6cff947dfa371c982c8a466a8bd7f39e
Fidan <lyonmowa@gmail.com>
2024-01-17 14:03:29 +0100

    test the header

--- src/app/Home/FirstPart.jsx
+++ src/app/Home/FirstPart.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Box from '@mui/material/Box'
+import  Typography  from '@mui/material/Typography'
 
 
 export default function FirstPart() {
   return (
     <Box className='firstPage'>
+      <Typography color='white'>Hello Motherfuckers</Typography>
    
     </Box>
   )
