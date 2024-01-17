272ae31dc5ed00ed6de83cdfb0c8fa6ac442c05c
Fidan <lyonmowa@gmail.com>
2024-01-17 16:05:34 +0100

    video check

--- src/app/Home/VideoPart.jsx
+++ src/app/Home/VideoPart.jsx
@@ -7,13 +7,14 @@ export default function VideoPart() {
     <Box 
     width="100%" height="100%" sx={{ backgroundColor: "#111111" }}
     >
-      <video src={require("./video/video.mp4")} autoPlay loop muted playsInline className="video" />
+      {/* <video src={require("./video/video.mp4")} autoPlay loop muted playsInline className="video" /> */}
 
-      {/* <div>
+       <div>
       <video playsInline autoPlay loop muted className="video">
         <source src="./video/video.mp4" type="video/mp4" />
       </video>
-      </div> */}
+      </div> 
+          
       
     </Box>
   );
